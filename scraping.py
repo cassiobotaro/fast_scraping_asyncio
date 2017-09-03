758f23805d87f3984d83cb3e76314a979b57a6d3
cassiobotaro <cassiobotaro@gmail.com>
2017-09-03 18:01:44 -0300

    bonus track

--- scraping.py
+++ scraping.py
@@ -1,3 +1,4 @@
+import tqdm
 import aiohttp
 import asyncio
 import bs4
@@ -22,9 +23,14 @@ async def print_magnet(query):
     magnet = first_magnet(page)
     print('{}: {}'.format(query, magnet))
 
+
+async def wait_with_progress(coros):
+    for f in tqdm.tqdm(asyncio.as_completed(coros), total=len(coros)):
+        await f
+
 distros = ['archlinux', 'ubuntu', 'debian', 'manjaro', 'suse', 'elementary',
            'fedora', 'gentoo']
 sem = asyncio.Semaphore(5)
 loop = asyncio.get_event_loop()
-f = asyncio.wait([print_magnet(d) for d in distros])
-loop.run_until_complete(f)
+f = [print_magnet(d) for d in distros]
+loop.run_until_complete(wait_with_progress(f))
